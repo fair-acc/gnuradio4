ab059d4b40d29aea05cc713c3b093df9b9057121
Matthias Kretz <m.kretz@gsi.de>
2022-12-14 20:38:26 +0100

    transferred initial concept
    from https://git.gsi.de/mkretz/graph-prototype/
    
    transferred by: Ralph J. Steinhagen <R.Steinhagen@GSI.de>

---
+++ include/typelist.hpp
@@ -0,0 +1,197 @@
+#ifndef GRAPH_PROTOTYPE_TYPELIST_HPP
+#define GRAPH_PROTOTYPE_TYPELIST_HPP
+
+#include <bit>
+#include <concepts>
+#include <type_traits>
+
+namespace fair::meta {
+template<typename... Ts>
+struct typelist;
+
+// concat ///////////////
+namespace detail {
+template<typename...>
+struct concat_impl;
+
+template<>
+struct concat_impl<> {
+    using type = typelist<>;
+};
+
+template<typename A>
+struct concat_impl<A> {
+    using type = typelist<A>;
+};
+
+template<typename... As>
+struct concat_impl<typelist<As...>> {
+    using type = typelist<As...>;
+};
+
+template<typename A, typename B>
+struct concat_impl<A, B> {
+    using type = typelist<A, B>;
+};
+
+template<typename... As, typename B>
+struct concat_impl<typelist<As...>, B> {
+    using type = typelist<As..., B>;
+};
+
+template<typename A, typename... Bs>
+struct concat_impl<A, typelist<Bs...>> {
+    using type = typelist<A, Bs...>;
+};
+
+template<typename... As, typename... Bs>
+struct concat_impl<typelist<As...>, typelist<Bs...>> {
+    using type = typelist<As..., Bs...>;
+};
+
+template<typename A, typename B, typename C>
+struct concat_impl<A, B, C> {
+    using type = typename concat_impl<typename concat_impl<A, B>::type, C>::type;
+};
+
+template<typename A, typename B, typename C, typename D, typename... More>
+struct concat_impl<A, B, C, D, More...> {
+    using type = typename concat_impl<typename concat_impl<A, B>::type,
+            typename concat_impl<C, D>::type,
+            typename concat_impl<More...>::type>::type;
+};
+} // namespace detail
+
+template<typename... Ts>
+using concat = typename detail::concat_impl<Ts...>::type;
+
+// split_at, left_of, right_of ////////////////
+namespace detail {
+template<unsigned N>
+struct splitter;
+
+template<>
+struct splitter<0> {
+    template<typename...>
+    using first = typelist<>;
+    template<typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<>
+struct splitter<1> {
+    template<typename T0, typename...>
+    using first = typelist<T0>;
+    template<typename, typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<>
+struct splitter<2> {
+    template<typename T0, typename T1, typename...>
+    using first = typelist<T0, T1>;
+    template<typename, typename, typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<>
+struct splitter<4> {
+    template<typename T0, typename T1, typename T2, typename T3, typename...>
+    using first = typelist<T0, T1, T2, T3>;
+    template<typename, typename, typename, typename, typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<>
+struct splitter<8> {
+    template<typename T0, typename T1, typename T2, typename T3, typename T4, typename T5,
+            typename T6, typename T7, typename...>
+    using first = typelist<T0, T1, T2, T3, T4, T5, T6, T7>;
+    template<typename, typename, typename, typename, typename, typename, typename, typename,
+            typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<>
+struct splitter<16> {
+    template<typename T0, typename T1, typename T2, typename T3, typename T4, typename T5,
+            typename T6, typename T7, typename T8, typename T9, typename T10, typename T11,
+            typename T12, typename T13, typename T14, typename T15, typename...>
+    using first = typelist<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+            T15>;
+    template<typename, typename, typename, typename, typename, typename, typename, typename,
+            typename, typename, typename, typename, typename, typename, typename, typename,
+            typename... Ts>
+    using second = typelist<Ts...>;
+};
+
+template<unsigned N>
+struct splitter {
+    static constexpr unsigned FirstSplit = std::has_single_bit(N) ? N / 2 : std::bit_floor(N);
+    using A                              = splitter<FirstSplit>;
+    using B                              = splitter<N - FirstSplit>;
+
+    template<typename... Ts>
+    using first = concat<typename A::template first<Ts...>,
+            typename B::template first<typename A::template second<Ts...>>>;
+
+    template<typename... Ts>
+    using second = typename B::template second<typename A::template second<Ts...>>;
+};
+} // namespace detail
+
+template<unsigned N, typename List>
+struct split_at;
+
+template<unsigned N, typename... Ts>
+struct split_at<N, typelist<Ts...>> {
+    using first  = typename detail::splitter<N>::template first<Ts...>;
+    using second = typename detail::splitter<N>::template second<Ts...>;
+};
+
+template<int N, typename List>
+using left_of = typename split_at<N, List>::first;
+
+template<int N, typename List>
+using right_of = typename split_at<N + 1, List>::second;
+
+// remove_at /////////////
+template<int Idx, typename List>
+using remove_at = concat<left_of<Idx, List>, right_of<Idx, List>>;
+
+// first_type ////////////
+namespace detail {
+template<typename List>
+struct first_type_impl {
+};
+
+template<typename T0, typename... Ts>
+struct first_type_impl<typelist<T0, Ts...>> {
+    using type = T0;
+};
+} // namespace detail
+
+template<typename List>
+using first_type = typename detail::first_type_impl<List>::type;
+
+// typelist /////////////////
+template<typename... Ts>
+struct typelist {
+    static inline constexpr std::integral_constant<int, sizeof...(Ts)> size = {};
+
+    template<template<typename...> class Other>
+    using apply = Other<Ts...>;
+
+    template<int I>
+    using at = first_type<typename detail::splitter<I>::template second<Ts...>>;
+
+    template<typename... Other>
+    static constexpr inline bool each_convertible_to = (std::convertible_to<Ts, Other> && ...);
+
+    template<typename... Other>
+    static constexpr inline bool each_convertible_from = (std::convertible_to<Other, Ts> && ...);
+};
+
+} // namespace fair::meta
+
+#endif // GRAPH_PROTOTYPE_TYPELIST_HPP
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <array>
+#include <fmt/core.h>
+
+#include "graph.hpp"
+
+template<typename T, int Depth>
+    requires(Depth > 0)
+class delay : public fair::graph::node<delay<T, Depth>, fair::graph::make_input_ports<T>, fair::graph::make_output_ports<T>> {
+    std::array<T, Depth> buffer = {};
+    int                  pos    = 0;
+
+public:
+    [[nodiscard]] constexpr T process_one(T in) noexcept {
+        T ret       = buffer[pos];
+        buffer[pos] = in;
+        if (pos == Depth - 1) {
+            pos = 0;
+        } else {
+            ++pos;
+        }
+        return ret;
+    }
+};
+
+template<typename T, typename R = decltype(std::declval<T>() + std::declval<T>())>
+class adder : public fair::graph::node<adder<T>, fair::graph::make_input_ports<T, T>, fair::graph::make_output_ports<R>> {
+public:
+    [[nodiscard]] constexpr R process_one(T a, T b) const noexcept { return a + b; }
+};
+
+template<typename T, T Scale, typename R = decltype(std::declval<T>() * std::declval<T>())>
+class scale : public fair::graph::node<scale<T, Scale, R>, fair::graph::make_input_ports<T>, fair::graph::make_output_ports<R>> {
+public:
+    [[nodiscard]] constexpr R process_one(T a) const noexcept { return a * Scale; }
+};
+
+int main() {
+    using fair::graph::merge;
+    // declare flow-graph: 2 x in -> adder -> scale-by-2 -> scale-by-minus1 -> output
+    auto merged = merge<0, 0>(scale<int, -1>(), merge<0, 0>(scale<int, 2>(), adder<int>()));
+
+    // execute graph
+    std::array<int, 4> a = { 1, 2, 3, 4 };
+    std::array<int, 4> b = { 10, 10, 10, 10 };
+
+    int                r = 0;
+    for (int i = 0; i < 4; ++i) {
+        auto [neg, scale, z] = merged.process_one(a[i], b[i]);
+        r += z;
+    }
+
+    return r;
+}
