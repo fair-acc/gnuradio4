b43fe91f289c55cc6ab912d3ce9c22f4ced277c4
Ralph J. Steinhagen <r.steinhagen@gsi.de>
2022-12-29 16:47:06 +0100

    added basic block and edge API definitions
    Co-authored-by: Ivan Čukić <ivan.cukic@kdab.com>

--- test/qa_port.cpp
+++ test/qa_port.cpp
@@ -1,11 +1,16 @@
-//import boost.ut;
 #include <boost/ut.hpp>
 
 #include <fmt/ranges.h>
 
 #include "buffer.hpp"
-#include "graph_contracts.h"
+#include "graph_contracts.hpp"
 #include "refl.hpp"
+
+struct test_block : fair::block {
+    test_block() : fair::block("test_block") {
+
+    }
+};
 
 const boost::ut::suite PortApiTests = [] {
     using namespace boost::ut;
@@ -26,38 +31,104 @@ const boost::ut::suite PortApiTests = [] {
     "PortBufferApi"_test = [] {
         OUT<float, "out0"> output_port;
         BufferWriter auto& writer = output_port.writer();
-        expect(ge(writer.available(), 32));
+        expect(ge(writer.available(), 32U));
 
         IN<float, "int0"> input_port;
-        BufferReader auto& reader = input_port.reader();
-        expect(eq(reader.available(), 0));
-        input_port.setBuffer(output_port.get_buffer());
+        const BufferReader auto& reader = input_port.reader();
+        expect(eq(reader.available(), 0U));
+        input_port.setBuffer(output_port.buffer());
 
-        expect(eq(output_port.get_buffer().n_readers(), 1));
+        expect(eq(output_port.buffer().n_readers(), 1U));
 
         int offset = 1;
         auto lambda = [&offset](auto& w) {
             std::iota(w.begin(), w.end(), offset);
-            fmt::print("generated output vector: {}\n", w);
+            fmt::print("typed-port connected output vector: {}\n", w);
             offset += w.size();
         };
 
-        writer.publish(lambda, 32);
         expect(writer.try_publish(lambda, 32));
     };
 
     "RuntimePortApi"_test = [] {
+        // declare in block
+        OUT<float, "out"> out;
+        IN<float, "in"> in;
         std::vector<dyn_port> port_list;
 
-        port_list.emplace_back(OUT<float, "out">());
-        port_list.emplace_back(IN<float, "in">());
+        port_list.emplace_back(out);
+        port_list.emplace_back(in);
 
-        for (int i=0; i < 3; i++) {
-            port_list.emplace_back(IN<float>(fmt::format("in{}", i)));
-        }
-        expect(eq(port_list.size(), 5));
+        expect(eq(port_list.size(), 2U));
+    };
+
+    "ConnectionApi"_test = [] {
+        using port_direction_t::INPUT;
+        using port_direction_t::OUTPUT;
+        OUT<float, "out0"> output_port;
+        BufferWriter auto& writer = output_port.writer();
+        IN<float, "in0"> input_port;
+
+        auto source = std::make_shared<block>("source");
+        source->add_port(output_port);
+        source->add_port(OUT<float, "out1">());
+        expect(eq(source->output_ports().size(), 2U));
+        expect(eq(source->input_ports().size(), 0U));
+
+        auto sink = std::make_shared<block>("sink");
+        expect(nothrow([&sink, &input_port] () { sink->add_port(input_port); } ));
+        expect(nothrow([&sink] () { sink->add_port(IN<float, "in1">()); } ));
+        expect(nothrow([&sink] () { sink->add_port(IN<float>("in2")); } ));
+        expect(throws([&sink] () { sink->add_port(IN<float>("in1")); } ));
+        expect(eq(sink->output_ports().size(), 0U));
+        expect(eq(sink->input_ports().size(), 3U));
+
+        std::vector<edge> edges;
+
+        expect(eq(edges.size(), 0U));
+
+        expect(eq(add_edge(edges, source, "out0", sink, "in0"), connection_result_t::SUCCESS));
+        // N.B. should discourage indexed-based access <-> error prone, we test this anyway
+        expect(eq(add_edge(edges, source, 0, sink, 1), connection_result_t::SUCCESS));
+
+        expect(eq(edges.size(), 2U));
+
+        auto mismatched_sink = std::make_shared<block>("mismatched_sink");
+        IN<int32_t, "in0"> mismatched_typed_port;
+        mismatched_sink->add_port(mismatched_typed_port);
+
+        expect(eq(add_edge(edges, source, 0, mismatched_sink, 0), connection_result_t::FAILED));
+
+        expect(eq(edges.size(), 2U));
+
+
+        // test runtime growing of src ports
+        expect(ge(writer.available(), 32U));
+        expect(eq(output_port.buffer().n_readers(), 0U));
+        const auto old_size = writer.available();
+        expect(eq(source->port<OUTPUT>("out0").value()->resize_buffer(old_size+1), connection_result_t::SUCCESS));
+        expect(gt(writer.available(), old_size)) << fmt::format("runtime increase of buffer size {} -> {}", old_size, writer.available());
+
+        // test runtime connection between ports
+        expect(eq(output_port.buffer().n_readers(), 0U));
+        expect(eq(source->port<OUTPUT>("out0").value()->connect(*sink->port<INPUT>("in0").value()), connection_result_t::SUCCESS)) << "fist connection";
+        expect(eq(source->port<OUTPUT>("out0").value()->connect(*sink->port<INPUT>("in0").value()), connection_result_t::SUCCESS)) << "double connection";
+        expect(eq(source->port<OUTPUT>("out0").value()->connect(*sink->port<INPUT>("in1").value()), connection_result_t::SUCCESS)) << "second connection";
+
+        const BufferReader auto& reader = input_port.reader();
+        expect(eq(reader.available(), 0U)) << fmt::format("reader already has some bytes pending: {}", reader.available());
+
+        expect(eq(output_port.buffer().n_readers(), 2U));
+
+        constexpr auto lambda = [](auto& w) {
+            std::iota(w.begin(), w.end(), 0U);
+            fmt::print("dyn_port connected sink output: {}\n", w);
+        };
+
+        expect(writer.try_publish(lambda, 32U));
+        expect(eq(input_port.reader().available(), 32U));
     };
 };
 
 
-int main() {}
+int main() { /* tests are statically executed */ }
