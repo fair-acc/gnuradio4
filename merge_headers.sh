7ac49ecea00bc6ca7562c4d1be38a8feee39b198
Alexander Krimm <A.Krimm@gsi.de>
2023-01-24 13:33:13 +0100

    update single header generation script and add ci
    The CI will always create a pseudo merge commit in the single-header
    branch, such that the branch will contain a parallel history to the main
    branch where each commit links the previous singleheader as its first
    parent and the commit from main as its second parent.

--- merge_headers.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/env bash
-mkdir -p singlehader
-# merge headers by precompiling toplevel headers to get list of included headers, filter for only lines with local headers, reverse the list to get deepest deps first
-# then concat all headers and filter out local includes and write to single header file
-cat $(gcc -std=c++20 -H include/graph.hpp 2>&1 | grep "^[\.][\.]* [^/]" | sed -e "s%^[\.]* %%" | tac ) include/graph.hpp | grep -v "#include \"" > singleheader/graph.hpp
-rm include/graph.hpp.gch # remove generated precompiled header
-
