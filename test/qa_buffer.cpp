3b917b8e1d6aed00726b675787c44d0880b14f15
Alexander Krimm <alex@wirew0rm.de>
2023-05-22 15:24:34 +0200

    tests: disable ut::reporter_junit for clang16
    clang 16 segfaults with the junit reporter due to some issues with
    cout streambuffer redirection. should be solved in upstream ut.

--- test/qa_buffer.cpp
+++ test/qa_buffer.cpp
@@ -14,6 +14,12 @@
 #include <sequence.hpp>
 #include <wait_strategy.hpp>
 
+#if defined(__clang__) && __clang_major__ >= 16
+// clang 16 does not like ut's default reporter_junit due to some issues with stream buffers and output redirection
+template <>
+auto boost::ut::cfg<boost::ut::override> = boost::ut::runner<boost::ut::reporter<>>{};
+#endif
+
 template<gr::WaitStrategy auto wait = gr::NoWaitStrategy()>
 struct TestStruct {
     [[nodiscard]] constexpr bool test() const noexcept { return true; }
--- test/qa_settings.cpp
+++ test/qa_settings.cpp
@@ -4,6 +4,12 @@
 #include <graph.hpp>
 #include <reflection.hpp>
 
+#if defined(__clang__) && __clang_major__ >= 16
+// clang 16 does not like ut's default reporter_junit due to some issues with stream buffers and output redirection
+template <>
+auto boost::ut::cfg<boost::ut::override> = boost::ut::runner<boost::ut::reporter<>>{};
+#endif
+
 namespace fair::graph::setting_test {
 
 template<typename T>
