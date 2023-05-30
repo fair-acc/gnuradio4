1a045498f99a44f99a2172cb50ae2f2bec252563
Alexander Krimm <A.Krimm@gsi.de>
2023-05-30 15:36:51 +0200

    qa_thread_pool: fix emscripten test
    use UT literals for better error reporting, correctly load atomics and
    add special case for thread name, which is not available on emscripten.

--- test/qa_thread_pool.cpp
+++ test/qa_thread_pool.cpp
@@ -20,29 +20,33 @@ const boost::ut::suite ThreadPoolTests = [] {
         expect(nothrow([&]{ pool.sleepDuration() = std::chrono::milliseconds(1); }));
         expect(nothrow([&]{ pool.keepAliveDuration() = std::chrono::seconds(10); }));
         pool.waitUntilInitialised();
-        expect(pool.isInitialised());
+        expect(that % pool.isInitialised());
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
         expect(pool.poolName() == "TestPool");
-        expect(pool.minThreads() == 1);
-        expect(pool.maxThreads() == 2);
-        expect(pool.numThreads() == 1);
-        expect(pool.numTasksRunning() == 0);
-        expect(pool.numTasksQueued() == 0);
-        expect(pool.numTasksRecycled() == 0);
+        expect(pool.minThreads() == 1_u);
+        expect(pool.maxThreads() == 2_u);
+        expect(pool.numThreads() == 1_u);
+        expect(pool.numTasksRunning() == 0_u);
+        expect(pool.numTasksQueued() == 0_u);
+        expect(pool.numTasksRecycled() == 0_u);
         pool.execute([&enqueueCount] { ++enqueueCount; enqueueCount.notify_all(); });
         enqueueCount.wait(0);
-        expect(pool.numThreads() == 1);
+        expect(pool.numThreads() == 1_u);
         pool.execute([&executeCount] { ++executeCount; executeCount.notify_all(); });
         executeCount.wait(0);
-        expect(pool.numThreads() >= 1);
-        expect(enqueueCount == 1);
-        expect(executeCount == 1);
+        expect(pool.numThreads() >= 1_u);
+        expect(enqueueCount.load() == 1_i);
+        expect(executeCount.load() == 1_i);
 
         auto ret = pool.execute([] { return 42; });
-        expect(ret.get() == 42);
+        expect(ret.get() == 42_i);
 
         auto taskName = pool.execute<"taskName", 0, -1>([] { return fair::thread_pool::thread::getThreadName(); });
-        expect(taskName.get() == "taskName");
+#ifdef __EMSCRIPTEN__
+        expect(taskName.get() == "unknown thread name"_b);
+#else
+        expect(taskName.get() == "taskName"_b);
+#endif
 
         expect(nothrow([&]{ pool.setAffinityMask(pool.getAffinityMask()); }));
         expect(nothrow([&]{ pool.setThreadSchedulingPolicy(pool.getSchedulingPolicy(), pool.getSchedulingPriority()); }));
@@ -51,15 +55,15 @@ const boost::ut::suite ThreadPoolTests = [] {
         std::atomic<int>                            counter{ 0 };
         fair::thread_pool::BasicThreadPool<fair::thread_pool::IO_BOUND> pool("contention", 1, 4);
         pool.waitUntilInitialised();
-        expect(pool.isInitialised());
-        expect(pool.numThreads() == 1);
+        expect(that % pool.isInitialised());
+        expect(pool.numThreads() == 1_u);
         pool.execute([&counter] { std::this_thread::sleep_for(std::chrono::milliseconds(10)); std::atomic_fetch_add(&counter, 1); counter.notify_all(); });
-        expect(pool.numThreads() == 1);
+        expect(pool.numThreads() == 1_u);
         pool.execute([&counter] { std::this_thread::sleep_for(std::chrono::milliseconds(10)); std::atomic_fetch_add(&counter, 1); counter.notify_all(); });
-        expect(pool.numThreads() >= 1);
+        expect(pool.numThreads() >= 1_u);
         counter.wait(0);
         counter.wait(1);
-        expect(counter == 2);
+        expect(counter.load() == 2_i);
     };
 };
 
