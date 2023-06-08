86f62f5b475e3f2de86da1202f7b33311a33ca72
Frank Osterfeld <frank.osterfeld@kdab.com>
2023-06-08 11:17:57 +0200

    WIP Add support for Triggered/PostMortem

--- include/data_sink.hpp
+++ include/data_sink.hpp
@@ -2,10 +2,12 @@
 #define GNURADIO_DATA_SINK_HPP
 
 #include "circular_buffer.hpp"
+#include "dataset.hpp"
 #include "node.hpp"
 #include "tag.hpp"
 
 #include <any>
+#include <ranges>
 
 namespace fair::graph {
 
@@ -57,6 +59,13 @@ public:
         std::lock_guard lg{mutex};
         auto sink = find_sink<T>(name);
         return sink ? sink->get_streaming_poller(block) : nullptr;
+    }
+
+    template<typename T, typename TriggerPredicate>
+    std::shared_ptr<typename data_sink<T>::dataset_poller> get_trigger_poller(std::string_view name, TriggerPredicate p, std::size_t pre_samples, std::size_t post_samples, blocking_mode block = blocking_mode::NonBlocking) {
+        std::lock_guard lg{mutex};
+        auto sink = find_sink<T>(name);
+        return sink ? sink->get_trigger_poller(std::move(p), pre_samples, post_samples, block) : nullptr;
     }
 
     template<typename T, typename Callback>
@@ -125,21 +134,49 @@ public:
         }
     };
 
-private:
-    struct listener {
-        acquisition_mode mode = acquisition_mode::Triggered;
-        std::pair<int64_t, int64_t> window; ///< window of data to return in relation to the matching tag position, e.g. [-2000, 3000] to obtain 2000 presamples and 3000 postsamples
-        std::size_t history_size = 0;
-        bool block = false;
-        int64_t drop_count = 0;
-        std::function<bool(fair::graph::tag_t::map_type)> trigger_predicate;
-        gr::circular_buffer<T> buffer;
-        std::optional<std::size_t> pending; ///< number of samples expected due to a previous trigger
-        std::function<void(std::span<const T>)> callback; // TODO we might want to pass back stats here like drop_count
-        std::weak_ptr<poller> polling_handler;
+    struct dataset_poller {
+        std::atomic<bool> finished = false;
+        std::atomic<std::size_t> drop_count = 0;
+        gr::circular_buffer<DataSet<T>> buffer = gr::circular_buffer<DataSet<T>>(listener_buffer_size); // TODO use other size?
+        decltype(buffer.new_reader()) reader = buffer.new_reader();
+        decltype(buffer.new_writer()) writer = buffer.new_writer();
+
+        template<typename Handler>
+        [[nodiscard]] bool process(Handler fnc) {
+            const auto available = reader.available();
+            if (available == 0) {
+                return false;
+            }
+
+            const auto read_data = reader.get(1);
+            fnc(read_data[0]);
+            reader.consume(1);
+            return true;
+        }
     };
 
-    std::vector<listener> listeners;
+private:
+    struct pending_window_t {
+        tag_t trigger;
+        DataSet<T> dataset;
+        std::size_t pending_post_samples = 0;
+    };
+
+    struct listener_t {
+        acquisition_mode mode = acquisition_mode::Triggered;
+        std::size_t pre_samples = 0;
+        std::size_t post_samples = 0;
+        bool block = false;
+        std::function<bool(fair::graph::tag_t)> trigger_predicate = {};
+        gr::circular_buffer<T> buffer = gr::circular_buffer<T>(0);
+        std::deque<pending_window_t> pending_trigger_windows; // triggers that still didn't receive all their data
+        std::function<void(std::span<const T>)> callback = {}; // TODO we might want to pass back stats here like drop_count
+        std::weak_ptr<dataset_poller> dataset_polling_handler = {};
+        std::weak_ptr<poller> polling_handler = {};
+        int64_t drop_count = 0;
+    };
+
+    std::vector<listener_t> listeners;
     std::mutex listener_mutex;
 
 public:
@@ -157,9 +194,24 @@ public:
         listeners.push_back({
             .mode = acquisition_mode::Continuous,
             .block = block == blocking_mode::Blocking,
-            .buffer = gr::circular_buffer<T>(0),
             .polling_handler = handler
         });
+        return handler;
+    }
+
+    template<typename TriggerPredicate>
+    std::shared_ptr<dataset_poller> get_trigger_poller(TriggerPredicate p, std::size_t pre_samples, std::size_t post_samples, blocking_mode block = blocking_mode::NonBlocking) {
+        std::lock_guard lg(listener_mutex);
+        auto handler = std::make_shared<dataset_poller>();
+        listeners.push_back({
+            .mode = acquisition_mode::Triggered,
+            .pre_samples = pre_samples,
+            .post_samples = post_samples,
+            .block = block == blocking_mode::Blocking,
+            .trigger_predicate = std::move(p),
+            .dataset_polling_handler = handler
+        });
+        history.resize(std::max(pre_samples, history.size()));
         return handler;
     }
 
@@ -168,9 +220,32 @@ public:
         std::lock_guard lg(listener_mutex);
         listeners.push_back({
             .mode = acquisition_mode::Continuous,
-            .buffer = gr::circular_buffer<T>(0),
             .callback = std::move(callback)
         });
+    }
+
+    // TODO this code should be called at the end of graph processing
+    void stop() {
+        std::lock_guard lg(listener_mutex);
+        for (auto &listener : listeners) {
+            if (listener.mode == acquisition_mode::Triggered || listener.mode == acquisition_mode::PostMortem) {
+                // send out any incomplete data windows
+                for (auto &window : listener.pending_trigger_windows) {
+                    publish_dataset(listener, std::move(window.dataset));
+                }
+
+                if (auto p = listener.dataset_polling_handler.lock()) {
+                    p->finished = true;
+                }
+            } else if (listener.mode == acquisition_mode::Continuous) {
+                if (auto p = listener.polling_handler.lock()) {
+                    // TODO pass remaining data
+                    p->finished = true;
+                }  else {
+                    // TODO pass remaining data to callback
+                }
+            }
+        }
     }
 
     [[nodiscard]] work_return_t work() {
@@ -183,35 +258,82 @@ public:
         }
 
         const auto noutput_items = std::min(listener_buffer_size, n_readable);
+        const auto reader_position = reader.position() + 1;
         const auto in_data = reader.get(noutput_items);
+        const auto history_view = std::span(history.begin(), history_available);
+        // TODO I'm not sure why the +1 in "reader.position() + 1". Bug or do I misunderstand?
+        assert(reader_position == n_samples_consumed);
+        assert(n_samples_consumed == std::round(in_data[0]));
+
+        auto &tag_reader = in_port.tagReader();
+        const auto n_tags = tag_reader.available();
+        const auto tag_data = tag_reader.get(n_tags);
+        std::vector<tag_t> tags(tag_data.begin(), tag_data.end());
+        auto out_of_range = [end_pos = reader_position + noutput_items](const auto &tag) {
+            return tag.index > end_pos;
+        };
+        std::erase_if(tags, out_of_range);
+        tag_reader.consume(tags.size());
 
         {
             std::lock_guard lg(listener_mutex);
             for (auto &listener : listeners) {
                 if (listener.mode == acquisition_mode::Continuous) {
-                    if (auto poller = listener.polling_handler.lock()) {
-                        auto writer = poller->buffer.new_writer();
-                        const auto read_data = reader.get(noutput_items);
-                        if (listener.block) {
-                            auto write_data = writer.reserve_output_range(noutput_items);
-                            std::copy(read_data.begin(), read_data.end(), write_data.begin());
-                            write_data.publish(write_data.size());
-                        } else {
-                            const auto can_write = writer.available();
-                            const auto to_write = std::min(read_data.size(), can_write);
-                            poller->drop_count += read_data.size() - can_write;
-                            if (to_write > 0) {
-                                auto write_data = writer.reserve_output_range(to_write);
-                                std::copy(read_data.begin(), read_data.begin() + to_write - 1, write_data.begin());
-                                write_data.publish(write_data.size());
-                            }
+                    write_to_listener(listener, std::vector<std::span<const T>>{in_data});
+                }  else if (listener.mode == acquisition_mode::Triggered || listener.mode == acquisition_mode::PostMortem) {
+                    namespace views = std::ranges::views;
+                    auto filtered = tags | views::filter(listener.trigger_predicate);
+                    for (const auto &trigger : filtered) {
+                        // TODO fill dataset with metadata etc.
+                        DataSet<T> dataset;
+                        dataset.timing_events = {{trigger}};
+                        dataset.signal_values.reserve(listener.pre_samples + listener.post_samples);
+                        listener.pending_trigger_windows.push_back({.trigger = trigger, .dataset = std::move(dataset), .pending_post_samples = listener.post_samples});
+                    }
+                    auto window = listener.pending_trigger_windows.begin();
+                    while (window != listener.pending_trigger_windows.end()) {
+                        auto &dataset = window->dataset;
+                        const auto window_offset = window->trigger.index - reader_position;
+
+                        if (window_offset >= 0 && dataset.signal_values.empty()) { // new trigger, write history
+                            // old history: pre-trigger data from previous in_data (if available)
+                            const auto old_history_size = std::max(static_cast<std::int64_t>(listener.pre_samples) - window_offset, std::int64_t{0});
+                            const auto available = std::min(static_cast<std::size_t>(old_history_size), history_view.size());
+                            const auto old_history_view = history_view.last(available);
+                            dataset.signal_values.insert(dataset.signal_values.end(), old_history_view.begin(), old_history_view.end());
+
+                            // new history: pre-trigger samples from the current in_data
+                            const auto new_history_size = listener.pre_samples - old_history_size;
+                            const auto new_history_view = in_data.subspan(window_offset - new_history_size, new_history_size);
+                            dataset.signal_values.insert(dataset.signal_values.end(), new_history_view.begin(), new_history_view.end());
                         }
-                    } else if (listener.callback) {
-                        listener.callback(in_data);
+
+                        // write missing post-samples
+                        const auto previous_post_samples = listener.post_samples - window->pending_post_samples;
+                        const auto first_requested = window_offset + previous_post_samples;
+                        const auto last_requested = window_offset + listener.post_samples - 1;
+                        const auto last_available = std::min(last_requested, noutput_items - 1);
+                        const auto post_sample_view = in_data.subspan(first_requested, last_available - first_requested + 1);
+                        dataset.signal_values.insert(dataset.signal_values.end(), post_sample_view.begin(), post_sample_view.end());
+                        window->pending_post_samples -= post_sample_view.size();
+
+                        if (window->pending_post_samples == 0) {
+                            publish_dataset(listener, std::move(dataset));
+                            window = listener.pending_trigger_windows.erase(window);
+                        } else {
+                            ++window;
+                        }
                     }
                 }
             }
+
+            // store potential pre-samples for triggers at the beginning of the next chunk
+            // TODO should use built-in history functionality that doesn't copy (but is resizable as listeners are added)
+            history_available = std::min(history.size(), noutput_items);
+            const auto history_data = in_data.last(history_available);
+            history.assign(history_data.begin(), history_data.end());
         }
+
 
         n_samples_consumed += noutput_items;
 
@@ -220,6 +342,75 @@ public:
         }
 
         return work_return_t::OK;
+    }
+
+private:
+    std::vector<T> history;
+    std::size_t history_available = 0;
+
+    inline void publish_dataset(listener_t &l, DataSet<T> &&data) {
+        if (auto poller = l.dataset_polling_handler.lock()) {
+            auto write_data = poller->writer.reserve_output_range(1);
+            if (l.block) {
+                write_data[0] = std::move(data);
+                write_data.publish(1);
+            } else {
+                if (poller->writer.available() > 0) {
+                    write_data[0] = std::move(data);
+                    write_data.publish(1);
+                } else {
+                    poller->drop_count++;
+                }
+            }
+        } else { // if callback...
+            // TODO call callback
+        }
+    }
+
+    inline void write_to_listener(listener_t &l, const std::vector<std::span<const T>> &spans) {
+        std::size_t total_size = 0;
+        for (const auto &i : spans) {
+            total_size += i.size();
+        }
+
+        if (total_size == 0) {
+            return;
+        }
+
+        if (auto poller = l.polling_handler.lock()) {
+            auto writer = poller->buffer.new_writer();
+            if (l.block) {
+                auto write_data = writer.reserve_output_range(total_size);
+                auto target = write_data.begin();
+                for (const auto &in_data : spans) {
+                    std::copy(in_data.begin(), in_data.end(), target);
+                    target += in_data.size();
+                }
+                write_data.publish(write_data.size());
+            } else {
+                const auto can_write = writer.available();
+                auto to_write = std::min(total_size, can_write);
+                poller->drop_count += total_size - can_write;
+                if (to_write > 0) {
+                    auto write_data = writer.reserve_output_range(to_write);
+                    std::size_t written = 0;
+                    for (const auto &in_data : spans) {
+                        const auto n = std::min(in_data.size(), to_write - written);
+                        std::copy(in_data.begin(), in_data.begin() + n, write_data.begin() + written);
+                        written += n;
+                        if (written == to_write) {
+                            break;
+                        }
+                    }
+                    write_data.publish(write_data.size());
+                }
+            }
+        } else if (l.callback) {
+            // TODO use buffer/make this one call
+            for (const auto &data : spans) {
+                l.callback(data);
+            }
+        }
     }
 };
 
--- test/qa_data_sink.cpp
+++ test/qa_data_sink.cpp
@@ -10,6 +10,7 @@
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
+#include <deque>
 #include <future>
 
 #if defined(__clang__) && __clang_major__ >= 16
@@ -20,7 +21,7 @@ auto boost::ut::cfg<boost::ut::override> = boost::ut::runner<boost::ut::reporter
 
 namespace fair::graph::data_sink_test {
 
-static constexpr std::int32_t n_samples = gr::util::round_up(1'000'000, 1024);
+static constexpr std::int32_t n_samples = 200000;
 
 template<typename T>
 struct Source : public node<Source<T>> {
@@ -30,6 +31,7 @@ struct Source : public node<Source<T>> {
     std::int32_t n_tag_offset       = 0;
     float        sample_rate        = 1000.0f;
     T            next_value         = {};
+    std::deque<tag_t> tags; // must be sorted by index
 
     void
     init(const tag_t::map_type &old_settings, const tag_t::map_type &new_settings) {
@@ -45,8 +47,17 @@ struct Source : public node<Source<T>> {
 
     [[nodiscard]] constexpr T
     process_one() noexcept {
+        while (!tags.empty() && tags[0].index == n_samples_produced) {
+            // TODO there probably is, or should be, an easier way to do this
+            const auto pos = output_port<"out">(this).streamWriter().position();
+            publish_tag(out, tags[0].map, n_samples_produced - pos);
+            tags.pop_front();
+        }
+
         n_samples_produced++;
-        return next_value++;
+        const auto v = next_value;
+        next_value++;
+        return v;
     }
 };
 
@@ -86,6 +97,9 @@ const boost::ut::suite DataSinkTests = [] {
     };
 
     "blocking polling continuous mode"_test = [] {
+
+        constexpr std::int32_t n_samples = 200000;
+
         graph flow_graph;
         auto &src = flow_graph.make_node<Source<float>>({ { "n_samples_max", n_samples } });
         auto &sink = flow_graph.make_node<data_sink<float>>();
@@ -103,14 +117,20 @@ const boost::ut::suite DataSinkTests = [] {
 
         auto make_runner = [](auto poller) {
             return std::async([poller] {
-                std::size_t samples_seen = 0;
-                while (!poller->finished) {
-                    [[maybe_unused]] auto r = poller->process([&samples_seen](const auto &data) {
-                        samples_seen += data.size();
-                    });
+                std::vector<float> received;
+                bool seen_finished = false;
+                while (!seen_finished) {
+                    // TODO make finished vs. pending data handling actually thread-safe
+                    seen_finished = poller->finished.load();
+                    while (poller->process([&received](const auto &data) {
+                        received.insert(received.end(), data.begin(), data.end());
+                    })) {}
                 }
 
-                expect(eq(samples_seen, n_samples));
+                std::vector<float> expected(n_samples);
+                std::iota(expected.begin(), expected.end(), 0.0);
+                expect(eq(received.size(), expected.size()));
+                expect(eq(received, expected));
                 expect(eq(poller->drop_count.load(), 0));
             });
         };
@@ -121,13 +141,111 @@ const boost::ut::suite DataSinkTests = [] {
         fair::graph::scheduler::simple sched{std::move(flow_graph)};
         sched.work();
 
-        poller1->finished = true; // TODO this should be done by the block
-        poller2->finished = true;
+        sink.stop(); // TODO the scheduler should call this
 
         runner1.wait();
         runner2.wait();
 
         expect(eq(sink.n_samples_consumed, n_samples));
+    };
+
+    "blocking polling trigger mode non-overlapping"_test = [] {
+        constexpr std::int32_t n_samples = 200000;
+
+        graph flow_graph;
+        auto &src = flow_graph.make_node<Source<float>>({ { "n_samples_max", n_samples } });
+        src.tags = {{3000, {{"TYPE", "TRIGGER"}}}, tag_t{8000, {{"TYPE", "NO_TRIGGER"}}}, {180000, {{"TYPE", "TRIGGER"}}}};
+        auto &sink = flow_graph.make_node<data_sink<float>>();
+        sink.set_name("test_sink");
+
+        expect(eq(connection_result_t::SUCCESS, flow_graph.connect<"out">(src).to<"in">(sink)));
+
+        auto is_trigger = [](const tag_t &tag) {
+            const auto v = tag.get("TYPE");
+            return v && std::get<std::string>(v->get()) == "TRIGGER";
+        };
+
+        auto poller = data_sink_registry::instance().get_trigger_poller<float>("test_sink", is_trigger, 3, 2, blocking_mode::Blocking);
+        expect(neq(poller, nullptr));
+
+        std::mutex m;
+        std::vector<float> received_data;
+
+        auto polling = std::async([poller, &received_data, &m] {
+            while (!poller->finished) {
+                using namespace std::chrono_literals;
+                [[maybe_unused]] auto r = poller->process([&received_data, &m](const auto &dataset) {
+                    std::lock_guard lg{m};
+                    received_data.insert(received_data.end(), dataset.signal_values.begin(), dataset.signal_values.end());
+                });
+            }
+        });
+
+        fair::graph::scheduler::simple sched{std::move(flow_graph)};
+        sched.work();
+
+        sink.stop(); // TODO the scheduler should call this
+
+        polling.wait();
+
+        std::lock_guard lg{m};
+        expect(eq(sink.n_samples_consumed, n_samples));
+        expect(eq(received_data.size(), 10));
+        expect(eq(received_data, std::vector<float>{2997, 2998, 2999, 3000, 3001, 179997, 179998, 179999, 180000, 180001}));
+        expect(eq(poller->drop_count.load(), 0));
+    };
+
+    "blocking polling trigger mode overlapping"_test = [] {
+        constexpr std::int32_t n_samples = 2000000;
+        constexpr std::size_t n_triggers = 5000;
+
+        graph flow_graph;
+        auto &src = flow_graph.make_node<Source<float>>({ { "n_samples_max", n_samples } });
+
+        for (std::size_t i = 0; i < n_triggers; ++i) {
+            src.tags.push_back(tag_t{60000L + static_cast<int64_t>(i), {{"TYPE", "TRIGGER"}}});
+        }
+
+        auto &sink = flow_graph.make_node<data_sink<float>>();
+        sink.set_name("test_sink");
+
+        expect(eq(connection_result_t::SUCCESS, flow_graph.connect<"out">(src).to<"in">(sink)));
+
+        auto is_trigger = [](const tag_t &tag) {
+            return true;
+        };
+
+        auto poller = data_sink_registry::instance().get_trigger_poller<float>("test_sink", is_trigger, 3000, 2000, blocking_mode::Blocking);
+        expect(neq(poller, nullptr));
+
+        std::mutex m;
+        std::vector<float> received_data;
+
+        auto polling = std::async([poller, &received_data, &m] {
+            while (!poller->finished) {
+                using namespace std::chrono_literals;
+                [[maybe_unused]] auto r = poller->process([&received_data, &m](const auto &dataset) {
+                    std::lock_guard lg{m};
+                    expect(eq(dataset.signal_values.size(), 5000));
+                    received_data.push_back(dataset.signal_values.front());
+                    received_data.push_back(dataset.signal_values.back());
+                });
+            }
+        });
+
+        fair::graph::scheduler::simple sched{std::move(flow_graph)};
+        sched.work();
+
+        sink.stop(); // TODO the scheduler should call this
+
+        polling.wait();
+
+        std::lock_guard lg{m};
+        auto expected_start = std::vector<float>{57000, 61999, 57001, 62000, 57002};
+        expect(eq(sink.n_samples_consumed, n_samples));
+        expect(eq(received_data.size(), 2 * n_triggers));
+        expect(eq(std::vector(received_data.begin(), received_data.begin() + 5), expected_start));
+        expect(eq(poller->drop_count.load(), 0));
     };
 
     "non-blocking polling continuous mode"_test = [] {
@@ -138,34 +256,38 @@ const boost::ut::suite DataSinkTests = [] {
 
         expect(eq(connection_result_t::SUCCESS, flow_graph.connect<"out">(src).to<"in">(sink)));
 
-        std::atomic<std::size_t> samples_seen = 0;
-
         auto invalid_type_poller = data_sink_registry::instance().get_streaming_poller<double>("test_sink");
         expect(eq(invalid_type_poller, nullptr));
 
         auto poller = data_sink_registry::instance().get_streaming_poller<float>("test_sink");
         expect(neq(poller, nullptr));
 
-        auto polling = std::async([poller, &samples_seen] {
+        auto polling = std::async([poller] {
             expect(neq(poller, nullptr));
-            while (!poller->finished) {
+            std::size_t samples_seen = 0;
+            bool seen_finished = false;
+            while (!seen_finished) {
+                // TODO make finished vs. pending data handling actually thread-safe
                 using namespace std::chrono_literals;
                 std::this_thread::sleep_for(20ms);
-                [[maybe_unused]] auto r = poller->process([&samples_seen](const auto &data) {
+
+                seen_finished = poller->finished.load();
+                while (poller->process([&samples_seen](const auto &data) {
                     samples_seen += data.size();
-                });
+                })) {}
             }
+
+            expect(eq(samples_seen + poller->drop_count.load(), n_samples));
         });
 
         fair::graph::scheduler::simple sched{std::move(flow_graph)};
         sched.work();
 
-        poller->finished = true; // TODO this should be done by the block
+        sink.stop(); // TODO the scheduler should call this
 
         polling.wait();
 
         expect(eq(sink.n_samples_consumed, n_samples));
-        expect(eq(samples_seen.load() + poller->drop_count.load(), n_samples));
     };
 };
 
