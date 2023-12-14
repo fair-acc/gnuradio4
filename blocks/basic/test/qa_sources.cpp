051af1e40b29bfc604ffaff15dcc340184f3d191
drslebedev <dr.s.lebedev@gmail.com>
2023-12-14 16:12:08 +0100

    Implementation of FunctionGenerator class for: Const, LinearRamp, ParabolicRamp, CubicSpline, ImpulseResponse functions

--- blocks/basic/test/qa_sources.cpp
+++ blocks/basic/test/qa_sources.cpp
@@ -7,6 +7,7 @@
 
 #include <gnuradio-4.0/algorithm/ImChart.hpp>
 #include <gnuradio-4.0/basic/clock_source.hpp>
+#include <gnuradio-4.0/basic/FunctionGenerator.hpp>
 #include <gnuradio-4.0/basic/SignalGenerator.hpp>
 #include <gnuradio-4.0/testing/TagMonitors.hpp>
 
@@ -15,6 +16,13 @@
 template<>
 auto boost::ut::cfg<boost::ut::override> = boost::ut::runner<boost::ut::reporter<>>{};
 #endif
+
+template<typename T>
+constexpr void
+addTimeTagEntry(gr::basic::ClockSource<T> &clockSource, std::uint64_t timeInNanoseconds, const std::string &value) {
+    clockSource.tag_times.push_back(timeInNanoseconds);
+    clockSource.tag_values.push_back(value);
+}
 
 const boost::ut::suite TagTests = [] {
     using namespace boost::ut;
@@ -163,6 +171,144 @@ const boost::ut::suite TagTests = [] {
         sched.runAndWait();
 
         expect(eq(n_samples, static_cast<std::uint32_t>(sink.n_samples_produced))) << "Number of samples does not match";
+    };
+
+    "FunctionGenerator ImChart test"_test = [] {
+        using namespace function_generator;
+        const std::size_t       N          = 128; // test points
+        double                  startValue = 10.;
+        double                  finalValue = 20.;
+        std::vector<SignalType> signals{ Const, LinearRamp, ParabolicRamp, CubicSpline, ImpulseResponse };
+        for (const auto &sig : signals) {
+            FunctionGenerator<double> funcGen{};
+            std::ignore = funcGen.settings().set({ createPropertyMapEntry(signal_type, sig),
+                                                   { "n_samples_max", static_cast<std::uint32_t>(N) },
+                                                   { "sample_rate", 128.f },
+                                                   createPropertyMapEntry(start_value, startValue),
+                                                   createPropertyMapEntry(final_value, finalValue),
+                                                   createPropertyMapEntry(duration, 1.),
+                                                   createPropertyMapEntry(round_off_time, .15),
+                                                   createPropertyMapEntry(impulse_time0, .2),
+                                                   createPropertyMapEntry(impulse_time1, .15) });
+            std::ignore = funcGen.settings().applyStagedParameters();
+
+            std::vector<double> xValues(N), yValues(N);
+            std::iota(xValues.begin(), xValues.end(), 0);
+            std::ranges::generate(yValues, [&funcGen]() { return funcGen.processOne(0.); });
+            fmt::println("Chart {}\n\n", toString(sig));
+            auto chart = gr::graphs::ImChart<128, 32>({ { 0., static_cast<double>(N) }, { 7., 22. } });
+            chart.draw(xValues, yValues, toString(sig));
+            chart.draw();
+        }
+    };
+
+    "FunctionGenerator + ClockSource test"_test = [] {
+        using namespace function_generator;
+        constexpr std::uint32_t N           = 1000;
+        constexpr float         sample_rate = 1000.f;
+        Graph                   testGraph;
+        auto                   &clockSrc = testGraph.emplaceBlock<gr::basic::ClockSource<float>>({ { "sample_rate", sample_rate }, { "n_samples_max", N }, { "name", "ClockSource" } });
+
+        clockSrc.tags = { Tag(0, createConstPropertyMap(5.f)),                              //
+                          Tag(100, createLinearRampPropertyMap(5.f, 30.f, .2f)),            //
+                          Tag(300, createConstPropertyMap(30.f)),                           //
+                          Tag(350, createParabolicRampPropertyMap(30.f, 20.f, .1f, 0.02f)), //
+                          Tag(550, createConstPropertyMap(20.f)),                           //
+                          Tag(650, createCubicSplinePropertyMap(20.f, 10.f, .1f)),          //
+                          Tag(800, createConstPropertyMap(10.f)),
+                          Tag(850, createImpulseResponsePropertyMap(10.f, 20.f, .02f, .06f)) };
+
+        auto &funcGen = testGraph.emplaceBlock<FunctionGenerator<float>>({ { "sample_rate", sample_rate }, { "name", "FunctionGenerator" } });
+        auto &sink    = testGraph.emplaceBlock<gr::testing::TagSink<float, ProcessFunction::USE_PROCESS_ONE>>({ { "name", "TagSink" } });
+
+        expect(eq(ConnectionResult::SUCCESS, testGraph.connect<"out">(clockSrc).to<"in">(funcGen)));
+        expect(eq(ConnectionResult::SUCCESS, testGraph.connect<"out">(funcGen).to<"in">(sink)));
+
+        scheduler::Simple sched{ std::move(testGraph) };
+        sched.runAndWait();
+        expect(eq(N, static_cast<std::uint32_t>(sink.samples.size()))) << "Number of samples does not match";
+
+        std::vector<double> xValues(N);
+        std::vector<double> yValues(sink.samples.begin(), sink.samples.end());
+        std::iota(xValues.begin(), xValues.end(), 0);
+        auto chart = gr::graphs::ImChart<128, 32>({ { 0., static_cast<double>(N) }, { 0., 35. } });
+        chart.draw(xValues, yValues, "Signal");
+        chart.draw();
+    };
+
+    "FunctionGenerator + ClockSource FAIR test"_test = [] {
+        using namespace function_generator;
+        constexpr std::uint32_t N           = 1000;
+        constexpr float         sample_rate = 10000.f;
+        Graph                   testGraph;
+        auto                   &clockSrc = testGraph.emplaceBlock<gr::basic::ClockSource<float>>({ { "sample_rate", sample_rate }, { "n_samples_max", N }, { "name", "ClockSource" } });
+
+        // all times are in nanoseconds
+        addTimeTagEntry(clockSrc, 1'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=1");
+        addTimeTagEntry(clockSrc, 10'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=2");
+        addTimeTagEntry(clockSrc, 30'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=3");
+        addTimeTagEntry(clockSrc, 35'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=4");
+        addTimeTagEntry(clockSrc, 55'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=5");
+        addTimeTagEntry(clockSrc, 65'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=6");
+        addTimeTagEntry(clockSrc, 80'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=7");
+        addTimeTagEntry(clockSrc, 85'000'000, "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=8");
+        clockSrc.repeat_period      = 5'000'000;
+        clockSrc.do_zero_order_hold = true;
+        auto &funcGen               = testGraph.emplaceBlock<FunctionGenerator<float>>({ { "sample_rate", sample_rate }, { "name", "FunctionGenerator" } });
+        funcGen.match_pred          = [](const auto &tableEntry, const auto &searchEntry, const auto attempt) -> std::optional<bool> {
+            if (searchEntry.find("context") == searchEntry.end()) {
+                return std::nullopt;
+            }
+            if (tableEntry.find("context") == tableEntry.end()) {
+                return false;
+            }
+
+            const pmtv::pmt searchEntryContext = searchEntry.at("context");
+            const pmtv::pmt tableEntryContext  = tableEntry.at("context");
+            if (std::holds_alternative<std::string>(searchEntryContext) && std::holds_alternative<std::string>(tableEntryContext)) {
+                const auto searchString = std::get<std::string>(searchEntryContext);
+                const auto tableString  = std::get<std::string>(tableEntryContext);
+
+                if (!searchString.starts_with("CMD_BP_START:")) {
+                    return std::nullopt;
+                }
+
+                if (attempt >= searchString.length()) {
+                    return std::nullopt;
+                }
+                auto [it1, it2] = std::ranges::mismatch(searchString, tableString);
+                if (std::distance(searchString.begin(), it1) == static_cast<std::ptrdiff_t>(searchString.length() - attempt)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        // Time duration is in seconds
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=1" } }, createConstPropertyMap(5.f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=2" } }, createLinearRampPropertyMap(5.f, 30.f, 0.02f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=3" } }, createConstPropertyMap(30.f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=4" } }, createParabolicRampPropertyMap(30.f, 20.f, .01f, 0.002f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=5" } }, createConstPropertyMap(20.f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=6" } }, createCubicSplinePropertyMap(20.f, 10.f, 0.01f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=7" } }, createConstPropertyMap(10.f));
+        funcGen.addFunctionTableEntry({ { "context", "CMD_BP_START:FAIR.SELECTOR.C=1:S=1:P=8" } }, createImpulseResponsePropertyMap(10.f, 20.f, 0.002f, 0.006f));
+
+        auto &sink = testGraph.emplaceBlock<gr::testing::TagSink<float, ProcessFunction::USE_PROCESS_ONE>>({ { "name", "TagSink" } });
+
+        expect(eq(ConnectionResult::SUCCESS, testGraph.connect<"out">(clockSrc).to<"in">(funcGen)));
+        expect(eq(ConnectionResult::SUCCESS, testGraph.connect<"out">(funcGen).to<"in">(sink)));
+
+        scheduler::Simple sched{ std::move(testGraph) };
+        sched.runAndWait();
+        expect(eq(N, static_cast<std::uint32_t>(sink.samples.size()))) << "Number of samples does not match";
+
+        std::vector<double> xValues(N);
+        std::vector<double> yValues(sink.samples.begin(), sink.samples.end());
+        std::iota(xValues.begin(), xValues.end(), 0);
+        auto chart = gr::graphs::ImChart<128, 32>({ { 0., static_cast<double>(N) }, { 0., 35. } });
+        chart.draw(xValues, yValues, "Signal");
+        chart.draw();
     };
 };
 
