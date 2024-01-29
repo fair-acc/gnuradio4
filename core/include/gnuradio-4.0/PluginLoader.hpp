3c111732967d45720c1c05d9f9c273064d85ce7d
Ivan Čukić <ivan.cukic@kdab.com>
2024-01-29 01:20:23 +0100

    Don't load already loaded plugins

--- core/include/gnuradio-4.0/PluginLoader.hpp
+++ core/include/gnuradio-4.0/PluginLoader.hpp
@@ -7,6 +7,7 @@
 #include <span>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -26,7 +27,7 @@ using namespace std::string_view_literals;
 #ifndef __EMSCRIPTEN__
 // Plugins are not supported on WASM
 
-class plugin_handler {
+class PluginHandler {
 private:
     void                     *_dl_handle  = nullptr;
     plugin_create_function_t  _create_fn  = nullptr;
@@ -49,9 +50,9 @@ private:
     }
 
 public:
-    plugin_handler() = default;
+    PluginHandler() = default;
 
-    explicit plugin_handler(const std::string &plugin_file) {
+    explicit PluginHandler(const std::string &plugin_file) {
         _dl_handle = dlopen(plugin_file.c_str(), RTLD_LAZY);
         if (!_dl_handle) {
             _status = "Failed to load the plugin file";
@@ -86,19 +87,19 @@ public:
         }
     }
 
-    plugin_handler(const plugin_handler &other) = delete;
-    plugin_handler &
-    operator=(const plugin_handler &other)
+    PluginHandler(const PluginHandler &other) = delete;
+    PluginHandler &
+    operator=(const PluginHandler &other)
             = delete;
 
-    plugin_handler(plugin_handler &&other) noexcept
+    PluginHandler(PluginHandler &&other) noexcept
         : _dl_handle(std::exchange(other._dl_handle, nullptr))
         , _create_fn(std::exchange(other._create_fn, nullptr))
         , _destroy_fn(std::exchange(other._destroy_fn, nullptr))
         , _instance(std::exchange(other._instance, nullptr)) {}
 
-    plugin_handler &
-    operator=(plugin_handler &&other) noexcept {
+    PluginHandler &
+    operator=(PluginHandler &&other) noexcept {
         auto tmp = std::move(other);
         std::swap(_dl_handle, tmp._dl_handle);
         std::swap(_create_fn, tmp._create_fn);
@@ -107,7 +108,7 @@ public:
         return *this;
     }
 
-    ~plugin_handler() { release(); }
+    ~PluginHandler() { release(); }
 
     explicit
     operator bool() const {
@@ -127,34 +128,37 @@ public:
 
 class PluginLoader {
 private:
-    std::vector<plugin_handler>                       _handlers;
-    std::unordered_map<std::string, gp_plugin_base *> _handler_for_name;
-    std::unordered_map<std::string, std::string>      _failed_plugins;
+    std::vector<PluginHandler>                        _handlers;
+    std::unordered_map<std::string, gp_plugin_base *> _handlerForName;
+    std::unordered_map<std::string, std::string>      _failedPlugins;
+    std::unordered_set<std::string>                   _loadedPluginFiles;
 
-    BlockRegistry           *_global_registry;
+    BlockRegistry           *_globalRegistry;
     std::vector<std::string> _knownBlocks;
 
 public:
-    PluginLoader(BlockRegistry *global_registry, std::span<const std::filesystem::path> plugin_directories) : _global_registry(global_registry) {
+    PluginLoader(BlockRegistry *global_registry, std::span<const std::filesystem::path> plugin_directories) : _globalRegistry(global_registry) {
         for (const auto &directory : plugin_directories) {
-            fmt::print("Plugin directory {}\n", directory.string());
             std::cerr << std::filesystem::current_path() << std::endl;
 
             if (!std::filesystem::is_directory(directory)) continue;
 
             for (const auto &file : std::filesystem::directory_iterator{ directory }) {
                 if (file.is_regular_file() && file.path().extension() == ".so") {
-                    fmt::print("Loading {}\n", file.path().string());
-                    if (plugin_handler handler(file.path().string()); handler) {
+                    auto fileString = file.path().string();
+                    if (_loadedPluginFiles.contains(fileString)) continue;
+                    _loadedPluginFiles.insert(fileString);
+
+                    if (PluginHandler handler(file.path().string()); handler) {
                         for (const auto &block_name : handler->providedBlocks()) {
-                            _handler_for_name.emplace(std::string(block_name), handler.operator->());
+                            _handlerForName.emplace(std::string(block_name), handler.operator->());
                             _knownBlocks.emplace_back(block_name);
                         }
 
                         _handlers.push_back(std::move(handler));
 
                     } else {
-                        _failed_plugins[file.path()] = handler.status();
+                        _failedPlugins[file.path()] = handler.status();
                     }
                 }
             }
@@ -168,13 +172,13 @@ public:
 
     const auto &
     failed_plugins() const {
-        return _failed_plugins;
+        return _failedPlugins;
     }
 
     auto
     knownBlocks() const {
         auto        result  = _knownBlocks;
-        const auto &builtin = _global_registry->knownBlocks();
+        const auto &builtin = _globalRegistry->knownBlocks();
         result.insert(result.end(), builtin.begin(), builtin.end());
         return result;
     }
@@ -182,11 +186,11 @@ public:
     std::unique_ptr<gr::BlockModel>
     instantiate(std::string_view name, std::string_view type, const property_map &params = {}) {
         // Try to create a node from the global registry
-        if (auto result = _global_registry->createBlock(name, type, params)) {
+        if (auto result = _globalRegistry->createBlock(name, type, params)) {
             return result;
         }
-        auto it = _handler_for_name.find(std::string(name)); // TODO avoid std::string here
-        if (it == _handler_for_name.end()) return {};
+        auto it = _handlerForName.find(std::string(name)); // TODO avoid std::string here
+        if (it == _handlerForName.end()) return {};
 
         auto &handler = it->second;
 
