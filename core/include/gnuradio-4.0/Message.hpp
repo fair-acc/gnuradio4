edbbcba5eb0014fee0285e6b9d85dad14ce87e85
Ivan Čukić <ivan.cukic@kdab.com>
2024-02-06 06:32:11 +0100

    wip

--- core/include/gnuradio-4.0/Message.hpp
+++ core/include/gnuradio-4.0/Message.hpp
@@ -20,10 +20,40 @@ const std::string ErrorInfo = "ERROR_INFO_KEY"; // optional: if a message has an
 
 namespace message::kind {
 const std::string Error           = "ERROR_KIND";
-const std::string Graph_update    = "GRAPH_UPDATE_KIND";
+const std::string GraphUpdate     = "GRAPH_UPDATE_KIND";
+const std::string SchedulerUpdate = "SCHEDULER_UPDATE_KIND";
 const std::string UpdateSettings  = "UPDATE_SETTINGS_KIND";
 const std::string SettingsChanged = "SETTINGS_CHANGED_KIND";
 } // namespace message::kind
+
+namespace message::graph_update {
+namespace what {
+const std::string BlockAdded         = "ADDED_BLOCK_GRAPH_UPDATE";
+const std::string BlockRemoved       = "REMOVED_BLOCK_GRAPH_UPDATE";
+const std::string BlockExchanged     = "EXCHANGED_BLOCK_GRAPH_UPDATE";
+const std::string BlocksConnected    = "CONNECTED_BLOCKS_GRAPH_UPDATE";
+const std::string BlocksDisconnected = "DISCONNECTED_BLOCKS_GRAPH_UPDATE";
+} // namespace what
+
+namespace data {
+const std::string AddedBlockId   = "ADDED_BLOCK_ID";  // for BlockAdded, BlockExchanged
+const std::string RemovedBlockId = "REMOVE_BLOCK_ID"; // for BlockRemoved, BlockExchanged
+
+const std::string SourceBlock      = "SOURCE_BLOCK";
+const std::string SourcePort       = "SOURCE_PORT";
+const std::string DestinationBlock = "DESTINATION_BLOCK";
+const std::string DestinationPort  = "DESTINATION_PORT";
+} // namespace data
+} // namespace message::graph_update
+
+namespace message::scheduler_update {
+const std::string SchedulerStarted  = "STARTED_SCHEDULER_UPDATE";
+const std::string SchedulerStarting = "STARTING_SCHEDULER_UPDATE";
+const std::string SchedulerPaused   = "PAUSED_SCHEDULER_UPDATE";
+const std::string SchedulerPausing  = "PAUSING_SCHEDULER_UPDATE";
+const std::string SchedulerStopped  = "STOPPED_SCHEDULER_UPDATE";
+const std::string SchedulerStopping = "STOPPING_SCHEDULER_UPDATE";
+} // namespace message::scheduler_update
 
 using Message = property_map;
 
