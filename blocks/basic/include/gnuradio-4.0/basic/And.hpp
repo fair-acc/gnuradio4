3a7865b4868e58cef3353d134e18e7fb6e55d30d
alexhojinpark <hojin7737@gmail.com>
2025-03-25 17:24:43 +0000

    Update And block to use std::integral and added empty line at the end
    Signed-off-by: alexhojinpark <hojin7737@gmail.com>

--- blocks/basic/include/gnuradio-4.0/basic/And.hpp
+++ blocks/basic/include/gnuradio-4.0/basic/And.hpp
@@ -3,13 +3,13 @@
 
 #include <gnuradio-4.0/Block.hpp>
 #include <gnuradio-4.0/BlockRegistry.hpp>
+#include <concepts>
 
 namespace gr::basic {
 
 GR_REGISTER_BLOCK(gr::basic::And, [uint8_t, int16_t, int32_t])
 
-template<typename T>
-requires std::is_arithmetic_v<T>
+template<std::integral T>
 struct And : Block<And<T>> {
     using Description = Doc<"@brief Performs a bitwise AND operation on two inputs, producing one output stream.">;
     
