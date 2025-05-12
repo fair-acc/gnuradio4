7caab3e84a357e0ce48500de69a82186d9c5ecea
drslebedev <dr.s.lebedev@gmail.com>
2025-05-12 10:48:15 +0200

    Add math block library to generated plugins * Remove obsolet MultiAdder block -> use gr::blocks::math::Add instead * Some fixes in math block registration
    Signed-off-by: drslebedev <dr.s.lebedev@gmail.com>
    Signed-off-by: Anmolmeet Singh <149612072+gitXsingh@users.noreply.github.com>

--- blocks/basic/include/gnuradio-4.0/basic/CommonBlocks.hpp
+++ blocks/basic/include/gnuradio-4.0/basic/CommonBlocks.hpp
@@ -57,47 +57,4 @@ struct builtin_counter : gr::Block<builtin_counter<T>> {
 template<typename T>
 gr::Size_t builtin_counter<T>::s_event_count = 0;
 
-template<typename T>
-struct MultiAdder : public gr::Block<MultiAdder<T>> {
-    std::vector<gr::PortIn<T>> inputs;
-    gr::PortOut<T>             out;
-
-    gr::Annotated<gr::Size_t, "n_inputs", gr::Visible, gr::Doc<"variable number of inputs">, gr::Limits<1U, 32U>> n_inputs{0U};
-
-    GR_MAKE_REFLECTABLE(MultiAdder, inputs, out, n_inputs);
-
-    void settingsChanged(const gr::property_map& old_settings, const gr::property_map& new_settings) {
-        if (new_settings.contains("n_inputs") && old_settings.at("n_inputs") != new_settings.at("n_inputs")) {
-            // if one of the port is already connected and  n_inputs was changed then throw
-            if (std::any_of(inputs.begin(), inputs.end(), [](const auto& port) { return port.isConnected(); })) {
-                this->emitErrorMessage("settingsChanged(..)", gr::Error("Number of input ports cannot be changed after Graph initialization."));
-            }
-            std::print("{}: configuration changed: n_inputs {} -> {}\n", this->name, old_settings.at("n_inputs"), new_settings.at("n_inputs"));
-            inputs.resize(n_inputs);
-        }
-    }
-
-    template<gr::InputSpanLike TInput>
-    gr::work::Status processBulk(std::span<TInput>& inSpans, gr::OutputSpanLike auto& outSpan) {
-        std::size_t minSizeIn = std::ranges::min_element(inSpans, [](const auto& lhs, const auto& rhs) { return lhs.size() < rhs.size(); })->size();
-        std::size_t available = std::min(outSpan.size(), minSizeIn);
-
-        if (available == 0) {
-            return gr::work::Status::INSUFFICIENT_INPUT_ITEMS;
-        }
-
-        for (std::size_t i = 0; i < available; i++) {
-            outSpan[i] = std::accumulate(inSpans.begin(), inSpans.end(), 0, [i](T sum, auto span) { return sum + span[i]; });
-        }
-        outSpan.publish(available);
-        for (auto& inSpan : inSpans) {
-            [[maybe_unused]] bool success = inSpan.consume(available);
-            assert(success && "Check that consume was successful");
-        }
-
-        return gr::work::Status::OK;
-    }
-};
-static_assert(gr::HasProcessBulkFunction<MultiAdder<float>>);
-
 #endif // include guard
