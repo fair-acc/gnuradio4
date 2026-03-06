ad057f7735b105e9a858651d105d4dc82965fd70
drslebedev <dr.s.lebedev@gmail.com>
2026-03-06 16:02:34 +0100

    Refactoring of HttpBlock: HttpSource + HttpSink, use FileIo internally.
    Signed-off-by: drslebedev <dr.s.lebedev@gmail.com>

--- blocks/http/test/qa_HttpBlock.cpp
+++ blocks/http/test/qa_HttpBlock.cpp
@@ -2,7 +2,13 @@
 
 #include <boost/ut.hpp>
 
+#include <atomic>
+#include <chrono>
 #include <format>
+#include <functional>
+#include <string>
+#include <thread>
+#include <vector>
 
 #include <gnuradio-4.0/Block.hpp>
 #include <gnuradio-4.0/Buffer.hpp>
@@ -14,23 +20,18 @@
 #include <gnuradio-4.0/http/HttpBlock.hpp>
 
 #include <gnuradio-4.0/meta/UnitTestHelper.hpp>
+#include <gnuradio-4.0/testing/TagMonitors.hpp>
 
-static_assert(gr::BlockLike<http::HttpBlock<uint8_t>>);
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 
-template<typename T>
-class FixedSource : public gr::Block<FixedSource<T>> {
-    using super_t = gr::Block<FixedSource<T>>;
+#ifndef __EMSCRIPTEN__
+#include <httplib.h>
+#endif
 
-public:
-    gr::PortOut<T> out;
-    T              value = 1;
-
-    GR_MAKE_REFLECTABLE(FixedSource, out);
-
-    [[nodiscard]] constexpr auto processOne() noexcept { return value; }
-
-    void trigger() { super_t::emitMessage("custom_kind", {}); }
-};
+static_assert(gr::BlockLike<gr::http::HttpSource>);
+static_assert(gr::BlockLike<gr::http::HttpSink>);
 
 template<typename T>
 class HttpTestSink : public gr::Block<HttpTestSink<T>> {
@@ -52,6 +53,10 @@ public:
     void reset() { value = {}; }
 };
 
+using ByteTagSource = gr::testing::TagSource<std::uint8_t, gr::testing::ProcessFunction::USE_PROCESS_BULK>;
+
+[[nodiscard]] gr::Tensor<std::uint8_t> byteTensor(std::string_view value) { return gr::Tensor<std::uint8_t>(value.begin(), value.end()); }
+
 const boost::ut::suite HttpBlocktests = [] {
     using namespace boost::ut;
     using namespace gr;
@@ -68,31 +73,25 @@ const boost::ut::suite HttpBlocktests = [] {
     "http GET"_test = [&] {
 #ifndef __EMSCRIPTEN__
         httplib::Server server;
-        server.Get("/echo", [](const httplib::Request, httplib::Response& res) { res.set_content("Hello world!", "text/plain"); });
+        server.Get("/echo", [](const httplib::Request&, httplib::Response& res) { res.set_content("Hello world!", "text/plain"); });
 
         auto thread = std::thread{[&server] { server.listen("localhost", 8080); }};
         server.wait_until_ready();
 #endif
 
         gr::Graph graph;
-        auto&     source    = graph.emplaceBlock<FixedSource<uint8_t>>();
-        auto&     httpBlock = graph.emplaceBlock<http::HttpBlock<uint8_t>>({{"url", gr::pmt::Value("http://localhost:8080")}, {"endpoint", gr::pmt::Value("/echo")}});
+        auto&     httpSource = graph.emplaceBlock<gr::http::HttpSource>({{"url", gr::pmt::Value("http://localhost:8080/echo")}});
+        auto&     sink       = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
 
-        auto& sink = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
-
-        expect(eq(ConnectionResult::SUCCESS, source.msgOut.connect(httpBlock.msgIn)));
-        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpBlock).template to<"in">(sink)));
+        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpSource).template to<"in">(sink)));
 
         gr::scheduler::Simple<> sched;
         if (auto ret = sched.exchange(std::move(graph)); !ret) {
             throw std::runtime_error(std::format("failed to initialize scheduler: {}", ret.error()));
         }
         sink.stopFunc = [&]() { expect(sched.changeStateTo(lifecycle::State::REQUESTED_STOP).has_value()); };
-        // make a request
-        source.trigger();
-        httpBlock.processScheduledMessages();
         expect(sched.runAndWait().has_value());
-        expect(eq(gr::test::get_value_or_fail<std::string>(sink.value.at("raw-data")), std::string("Hello world!")));
+        expect(eq(gr::test::get_value_or_fail<gr::Tensor<std::uint8_t>>(sink.value.at("raw-data")), byteTensor("Hello world!")));
 
 #ifndef __EMSCRIPTEN__
         server.stop();
@@ -103,28 +102,26 @@ const boost::ut::suite HttpBlocktests = [] {
     "http GET 404"_test = [&] {
 #ifndef __EMSCRIPTEN__
         httplib::Server server;
-        server.Get("/does-not-exist", [](const httplib::Request, httplib::Response& res) { res.status = httplib::StatusCode::NotFound_404; });
+        server.Get("/does-not-exist", [](const httplib::Request&, httplib::Response& res) { res.status = httplib::StatusCode::NotFound_404; });
 
         auto thread = std::thread{[&server] { server.listen("localhost", 8080); }};
         server.wait_until_ready();
 #endif
 
         gr::Graph graph;
-        auto&     source    = graph.emplaceBlock<FixedSource<uint8_t>>();
-        auto&     httpBlock = graph.emplaceBlock<http::HttpBlock<uint8_t>>(property_map{{"url", gr::pmt::Value("http://localhost:8080")}, {"endpoint", gr::pmt::Value("/does-not-exist")}});
-        auto&     sink      = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
+        auto&     httpSource = graph.emplaceBlock<gr::http::HttpSource>({{"url", gr::pmt::Value("http://localhost:8080/does-not-exist")}});
+        auto&     sink       = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
 
-        expect(eq(ConnectionResult::SUCCESS, source.msgOut.connect(httpBlock.msgIn)));
-        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpBlock).template to<"in">(sink)));
+        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpSource).template to<"in">(sink)));
 
+        gr::MsgPortIn           fromScheduler;
         gr::scheduler::Simple<> sched;
         if (auto ret = sched.exchange(std::move(graph)); !ret) {
             throw std::runtime_error(std::format("failed to initialize scheduler: {}", ret.error()));
         }
-        sink.stopFunc = [&]() { expect(sched.changeStateTo(lifecycle::State::REQUESTED_STOP).has_value()); };
-        httpBlock.trigger();
+        expect(eq(ConnectionResult::SUCCESS, sched.msgOut.connect(fromScheduler)));
         expect(sched.runAndWait().has_value());
-        expect(eq(gr::test::get_value_or_fail<int>(sink.value.at("status")), 404));
+        expect(eq(sched.state(), lifecycle::State::ERROR));
 
 #ifndef __EMSCRIPTEN__
         server.stop();
@@ -134,31 +131,31 @@ const boost::ut::suite HttpBlocktests = [] {
 
     "http POST"_test = [] {
 #ifndef __EMSCRIPTEN__
+        std::string     receivedBody;
         httplib::Server server;
-        server.Post("/number", [](const httplib::Request&, httplib::Response& res) { res.set_content("OK", "text/plain"); });
+        server.Post("/number", [&](const httplib::Request& req, httplib::Response& res) {
+            receivedBody = req.body;
+            res.set_content("OK", "text/plain");
+        });
 
         auto thread = std::thread{[&server] { server.listen("localhost", 8080); }};
         server.wait_until_ready();
 #endif
 
         gr::Graph graph;
-        auto&     source    = graph.emplaceBlock<FixedSource<uint8_t>>();
-        auto&     httpBlock = graph.emplaceBlock<http::HttpBlock<uint8_t>>({{"url", gr::pmt::Value("http://localhost:8080")}, {"endpoint", gr::pmt::Value("/number")}, {"type", gr::pmt::Value("POST")}, {"parameters", gr::pmt::Value("param=42")}});
-        auto&     sink      = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
+        auto&     source = graph.emplaceBlock<ByteTagSource>({{"values", byteTensor("param=42")}, {"n_samples_max", gr::Size_t(8)}});
+        auto&     sink   = graph.emplaceBlock<gr::http::HttpSink>({{"url", gr::pmt::Value("http://localhost:8080/number")}, {"content_type", gr::pmt::Value("application/x-www-form-urlencoded")}});
 
-        expect(eq(ConnectionResult::SUCCESS, source.msgOut.connect(httpBlock.msgIn)));
-        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpBlock).template to<"in">(sink)));
+        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(source).template to<"in">(sink)));
 
         gr::scheduler::Simple<> sched;
         if (auto ret = sched.exchange(std::move(graph)); !ret) {
             throw std::runtime_error(std::format("failed to initialize scheduler: {}", ret.error()));
         }
-        sink.stopFunc = [&]() { expect(sched.changeStateTo(lifecycle::State::REQUESTED_STOP).has_value()); };
-        httpBlock.trigger();
         expect(sched.runAndWait().has_value());
-        expect(eq(gr::test::get_value_or_fail<std::string>(sink.value.at("raw-data")), "OK"sv));
 
 #ifndef __EMSCRIPTEN__
+        expect(eq(receivedBody, "param=42"s));
         server.stop();
         thread.join();
 #endif
@@ -168,18 +165,13 @@ const boost::ut::suite HttpBlocktests = [] {
 #ifndef __EMSCRIPTEN__
         std::atomic_bool shutdown = false;
         httplib::Server  server;
-        server.Get("/notify", [&](const httplib::Request, httplib::Response& res) {
-            res.set_chunked_content_provider("text/plain", [&](size_t, httplib::DataSink& sink) {
-                if (shutdown) {
-                    return false;
-                }
-                // delay the reply a bit, we are long polling
-                std::this_thread::sleep_for(10ms);
-                if (sink.is_writable()) {
-                    sink.os << "event";
-                }
-                return true;
-            });
+        server.Get("/notify", [&](const httplib::Request&, httplib::Response& res) {
+            if (shutdown.load()) {
+                res.status = httplib::StatusCode::ServiceUnavailable_503;
+                return;
+            }
+            std::this_thread::sleep_for(10ms);
+            res.set_content("event", "text/plain");
         });
 
         auto thread = std::thread{[&server] { server.listen("localhost", 8080); }};
@@ -187,12 +179,10 @@ const boost::ut::suite HttpBlocktests = [] {
 #endif
 
         gr::Graph graph;
-        auto&     source    = graph.emplaceBlock<FixedSource<uint8_t>>();
-        auto&     httpBlock = graph.emplaceBlock<http::HttpBlock<uint8_t>>({{"url", gr::pmt::Value("http://localhost:8080")}, {"endpoint", gr::pmt::Value("/notify")}, {"type", gr::pmt::Value("SUBSCRIBE")}});
-        auto&     sink      = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
+        auto&     httpSource = graph.emplaceBlock<gr::http::HttpSource>({{"url", gr::pmt::Value("http://localhost:8080/notify")}, {"type", gr::pmt::Value("SUBSCRIBE")}});
+        auto&     sink       = graph.emplaceBlock<HttpTestSink<pmt::Value::Map>>();
 
-        expect(eq(ConnectionResult::SUCCESS, source.msgOut.connect(httpBlock.msgIn)));
-        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpBlock).template to<"in">(sink)));
+        expect(eq(ConnectionResult::SUCCESS, graph.connect<"out">(httpSource).template to<"in">(sink)));
 
         gr::scheduler::Simple<> sched;
         if (auto ret = sched.exchange(std::move(graph)); !ret) {
@@ -200,7 +190,7 @@ const boost::ut::suite HttpBlocktests = [] {
         }
         sink.stopFunc = [&]() { expect(sched.changeStateTo(lifecycle::State::REQUESTED_STOP).has_value()); };
         expect(sched.runAndWait().has_value());
-        expect(eq(gr::test::get_value_or_fail<std::string>(sink.value.at("raw-data")), "event"sv));
+        expect(eq(gr::test::get_value_or_fail<gr::Tensor<std::uint8_t>>(sink.value.at("raw-data")), byteTensor("event")));
 
 #ifndef __EMSCRIPTEN__
         shutdown = true;
