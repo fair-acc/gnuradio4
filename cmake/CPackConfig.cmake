822302a7e2b0c12cc631b12fc7229355071359a1
Daniel Nicoletti <daniel.nicoletti@kdab.com>
2026-04-14 15:45:14 -0300

    CPack fixes due /opt/gnuradio4
    Build a deb for emiscripten too
    
    Signed-off-by: Daniel Nicoletti <daniel.nicoletti@kdab.com>

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -2,7 +2,9 @@ set(CPACK_PACKAGE_VENDOR "GNURadio")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Prototype implementations for a more compile-time efficient flowgraph API")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_PACKAGE_CONTACT "admin@gnuradio.org")
-set(CPACK_PACKAGE_NAME "${PROJECT_NAME}${PROJECT_VERSION_MAJOR}")
+if(NOT CPACK_PACKAGE_NAME)
+  set(CPACK_PACKAGE_NAME "${PROJECT_NAME}${PROJECT_VERSION_MAJOR}")
+endif()
 
 if(UNIX)
   if(NOT CPACK_GENERATOR)
@@ -11,6 +13,9 @@ if(UNIX)
 
   set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
   set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+  # When installing to a non-standard prefix (e.g. /opt/gnuradio4), dpkg-shlibdeps
+  # cannot find the package's own private libraries. Point it to the install lib dir.
+  set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS_PRIVATE_DIRS "${CMAKE_INSTALL_PREFIX}/lib")
   if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo")
     set(CPACK_DEBIAN_DEBUGINFO_PACKAGE ON)
   endif()
